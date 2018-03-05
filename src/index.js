614dc477b50cdf4debc036994b6315814c6bc464
Marciuzz <martynas.girzadas@gmail.com>
2018-03-05 13:23:55 +0200

    initial commit added modules, configured webpack, babel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div>
+    <h1>Hello react</h1>
+  </div>,
+  document.getElementById('app')
+)
+
+export default render;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname + '/public/',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  mode: 'development',
+  watch: true
+};
